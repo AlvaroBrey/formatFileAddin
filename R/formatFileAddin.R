3a01ec319d41c4adc891ff26e7b96101d8a1a3a7
Álvaro Brey <alvaro.brv@gmail.com>
2017-11-03 12:00:45 +0100

    '=' -> '<-'

--- R/formatFileAddin.R
+++ R/formatFileAddin.R
@@ -11,11 +11,11 @@ reformatFileAddin <- function() {
 
     # Check if it's a R file
     sp <- strsplit(x = docPath, split = ".", fixed = TRUE)
-    t = sp[[1]]
-    ext = t[length(t)]
+    t <- sp[[1]]
+    ext <- t[length(t)]
     if (length(ext) && tolower(ext) == "r") {
         # Reformat it
-        output = paste(capture.output(formatR::tidy_source(text = docContents)), collapse = "\n")
+        output <- paste(capture.output(formatR::tidy_source(text = docContents)), collapse = "\n")
         rstudioapi::setDocumentContents(output, docId)
     } else {
         print("formatFile: Only R files are supported. Please focus on an editor with a R file open.")
