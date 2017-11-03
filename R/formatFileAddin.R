4f72608335e4d2e0d7349ece38b712d4fcbd3da1
Álvaro Brey <alvaro.brv@gmail.com>
2017-11-03 09:33:27 +0100

    Check if target is a R file first

--- R/formatFileAddin.R
+++ R/formatFileAddin.R
@@ -7,8 +7,17 @@ reformatFileAddin <- function() {
     context <- rstudioapi::getActiveDocumentContext()
     docId <- context$id
     docContents <- context$contents
+    docPath <- context$path
 
-    # Reformat it
-    output = paste(capture.output(formatR::tidy_source(text = docContents)), collapse = "\n")
-    rstudioapi::setDocumentContents(output, docId)
+    # Check if it's a R file
+    sp <- strsplit(x = docPath, split = ".", fixed = TRUE)
+    t = sp[[1]]
+    ext = t[length(t)]
+    if (length(ext) && tolower(ext) == "r") {
+        # Reformat it
+        output = paste(capture.output(formatR::tidy_source(text = docContents)), collapse = "\n")
+        rstudioapi::setDocumentContents(output, docId)
+    } else {
+        print("formatFile: Only R files are supported. Please focus on an editor with a R file open.")
+    }
 }
